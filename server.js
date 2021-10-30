d1c2b6c22af45a06b726b066d786a4c06528880c
lamperouge <philbohn1791@gmail.com>
2021-10-30 09:59:13 -0400

    added the app.listed to server.js

--- server.js
+++ server.js
@@ -19,3 +19,7 @@ mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker",
   { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
 );
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
